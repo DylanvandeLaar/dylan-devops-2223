d9a45e9f4ff6026127382e17930989986061c8fe
Dylan van de Laar <dcc.vandelaar@student.avans.nl>
2024-06-14 18:45:20 +0200

    added grafana

--- DevopsApp/routes/users.js
+++ DevopsApp/routes/users.js
@@ -1,19 +1,22 @@
-var express = require('express');
+const express = require('express');
+const router = express.Router();
+const {db} = require('../services/database');
+const publisher = require('../publisher');
 
-var router = express.Router();
-
-const { db } = require("../services/database");
-
-/* GET users listing. */
 router.get('/', async function(req, res, next) {
-  let users = await db.collection('users').find().toArray();
+  publisher.sendMessageToQueue('Retrieve users from the database');
+
+  const users = await db.collection('users').find().toArray();
+
   res.json(users);
 });
 
-router.post('/', function(req, res, next){
+router.post('/', function(req, res, next) {
+  publisher.sendMessageToQueue('Add new user to the database');
+
   db.collection('users').insertOne(req.body)
-    .then((user) => res.status(201).json({ "id": user.insertedId }))
-    .catch(err => res.status(500).json(err));
-})
+    .then((user) => res.status(201).json({'id': user.insertedId}))
+    .catch((err) => res.status(500).json(err));
+});
 
 module.exports = router;
--- LoggingAPI/routes/index.js
+++ LoggingAPI/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('this is the wordle logsAPI');
+  res.send('this is the devops logsAPI');
 });
 
 module.exports = router;
